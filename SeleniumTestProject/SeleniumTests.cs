92dc3bb0cd65cf735cbb79b697701b04d5b36e8d
Andrey Vlasov <lion32211@yandex.ru>
2021-09-25 16:20:09 +0300

    Added "NameOfItem" property in "ConfigElements" class

--- SeleniumTestProject/SeleniumTests.cs
+++ SeleniumTestProject/SeleniumTests.cs
@@ -15,6 +15,7 @@ namespace SeleniumTestProject
         public string ItemForSearch { get; set; }
         public string SearchButton { get; set; }
         public string SelectedItemInSearch { get; set; }
+        public string NameOfItem { get; set; }
         public string AddToBasketButton { get; set; }
         public string BasketButton { get; set; }
         public string ItemInBasket { get; set; }
