3d937c84004a32267524e8b474b701d4675575f0
Andrey Vlasov <lion32211@yandex.ru>
2021-10-06 14:06:23 +0300

    Relocated the ConfigElements class to a file for more flexibility

---
+++ SeleniumTestProject/ConfigElements.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SeleniumTestProject
+{
+    class ConfigElements
+    {
+        public string UrlAdress { get; set; }
+        public string SearchField { get; set; }
+        public string ItemForSearch { get; set; }
+        public string SearchButton { get; set; }
+        public string SelectedItemInSearch { get; set; }
+        public string NameOfItem { get; set; }
+        public string AddToBasketButton { get; set; }
+        public string BasketButton { get; set; }
+        public string ItemInBasket { get; set; }
+    }
+}
