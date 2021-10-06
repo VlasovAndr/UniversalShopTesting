f89128fb9c135c393b81748ba17399e31c37d69e
Andrey Vlasov <lion32211@yandex.ru>
2021-10-06 14:22:37 +0300

    Changed the structure of configuration file in accordance with Page Object Strategy

--- SeleniumTestProject/SeleniumTests.cs
+++ SeleniumTestProject/SeleniumTests.cs
@@ -21,25 +21,28 @@ namespace SeleniumTestProject
         public void UniversalShopTesting(string pathToConfigFile)
         {
 
-            var configElements = JsonConvert.DeserializeObject<Website>(File.ReadAllText(pathToConfigFile));
+            var wensite = JsonConvert.DeserializeObject<Website>(File.ReadAllText(pathToConfigFile));
             ChromeOptions options = new ChromeOptions();
             options.AddArguments("--disable-notifications");
             options.PageLoadStrategy = PageLoadStrategy.Eager;
             IWebDriver driver = new ChromeDriver(options);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-            driver.Navigate().GoToUrl(configElements.UrlAdress);
+            driver.Navigate().GoToUrl(wensite.Url);
             driver.Manage().Window.Maximize();
             Thread.Sleep(3000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(configElements.SearchField))).SendKeys(configElements.ItemForSearch + Keys.Enter);
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.StartPage.SearchField))).
+                SendKeys(wensite.Pages.StartPage.ItemForSearch + Keys.Enter);
             Thread.Sleep(2000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(configElements.SelectedItemInSearch))).Click();
-            var expectedResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(configElements.NameOfItem))).Text.ToLower().Replace(",", string.Empty);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(configElements.AddToBasketButton))).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductListPage.SelectedItem))).Click();
+            var expectedResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductDetailsPage.NameOfItem))).
+                Text.ToLower().Replace(",", string.Empty);
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductDetailsPage.AddToBasketButton))).Click();
             Thread.Sleep(2000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(configElements.BasketButton))).Click();
-            var actualResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(configElements.ItemInBasket))).Text.ToLower().Replace(",", string.Empty);
-            var actualCount = driver.FindElements(By.XPath(configElements.ItemInBasket)).Count;
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductDetailsPage.BasketButton))).Click();
+            var actualResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.BasketPage.NameOfItem)))
+                .Text.ToLower().Replace(",", string.Empty);
+            var actualCount = driver.FindElements(By.XPath(wensite.Pages.BasketPage.NameOfItem)).Count;
 
             Assert.Contains(actualResult, expectedResult);
             Assert.Equal(1, actualCount);
--- SeleniumTestProject/Website.cs
+++ SeleniumTestProject/Website.cs
@@ -1,16 +1,40 @@
 ﻿
 namespace SeleniumTestProject
 {
-    class Website
+    public class Website
     {
-        public string UrlAdress { get; set; }
+        public string Url { get; set; }
+        public Pages Pages { get; set; }
+    }
+
+    public class Pages
+    {
+        public StartPage StartPage { get; set; }
+        public ProductListPage ProductListPage { get; set; }
+        public ProductDetailsPage ProductDetailsPage { get; set; }
+        public BasketPage BasketPage { get; set; }
+    }
+
+    public class StartPage
+    {
         public string SearchField { get; set; }
         public string ItemForSearch { get; set; }
-        public string SearchButton { get; set; }
-        public string SelectedItemInSearch { get; set; }
+    }
+
+    public class ProductListPage
+    {
+        public string SelectedItem { get; set; }
+    }
+
+    public class ProductDetailsPage
+    {
         public string NameOfItem { get; set; }
         public string AddToBasketButton { get; set; }
         public string BasketButton { get; set; }
-        public string ItemInBasket { get; set; }
+    }
+
+    public class BasketPage
+    {
+        public string NameOfItem { get; set; }
     }
 }
