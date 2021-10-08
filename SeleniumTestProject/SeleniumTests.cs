fa01f7fbc9a42e825fab6c74974def034e4a134e
Andrey Vlasov <lion32211@yandex.ru>
2021-10-08 16:14:21 +0300

    Fiexd the name of the class instance

--- SeleniumTestProject/SeleniumTests.cs
+++ SeleniumTestProject/SeleniumTests.cs
@@ -21,28 +21,28 @@ namespace SeleniumTestProject
         public void UniversalShopTesting(string pathToConfigFile)
         {
 
-            var wensite = JsonConvert.DeserializeObject<Website>(File.ReadAllText(pathToConfigFile));
+            var website = JsonConvert.DeserializeObject<Website>(File.ReadAllText(pathToConfigFile));
             ChromeOptions options = new ChromeOptions();
             options.AddArguments("--disable-notifications");
             options.PageLoadStrategy = PageLoadStrategy.Eager;
             IWebDriver driver = new ChromeDriver(options);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-            driver.Navigate().GoToUrl(wensite.Url);
+            driver.Navigate().GoToUrl(website.Url);
             driver.Manage().Window.Maximize();
             Thread.Sleep(3000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.StartPage.SearchField))).
-                SendKeys(wensite.Pages.StartPage.ItemForSearch + Keys.Enter);
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(website.Pages.StartPage.SearchField))).
+                SendKeys(website.Pages.StartPage.ItemForSearch + Keys.Enter);
             Thread.Sleep(2000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductListPage.SelectedItem))).Click();
-            var expectedResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductDetailsPage.NameOfItem))).
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(website.Pages.ProductListPage.SelectedItem))).Click();
+            var expectedResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(website.Pages.ProductDetailsPage.NameOfItem))).
                 Text.ToLower().Replace(",", string.Empty);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductDetailsPage.AddToBasketButton))).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(website.Pages.ProductDetailsPage.AddToBasketButton))).Click();
             Thread.Sleep(2000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.ProductDetailsPage.BasketButton))).Click();
-            var actualResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(wensite.Pages.BasketPage.NameOfItem)))
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(website.Pages.ProductDetailsPage.BasketButton))).Click();
+            var actualResult = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(website.Pages.BasketPage.NameOfItem)))
                 .Text.ToLower().Replace(",", string.Empty);
-            var actualCount = driver.FindElements(By.XPath(wensite.Pages.BasketPage.NameOfItem)).Count;
+            var actualCount = driver.FindElements(By.XPath(website.Pages.BasketPage.NameOfItem)).Count;
 
             Assert.Contains(actualResult, expectedResult);
             Assert.Equal(1, actualCount);
